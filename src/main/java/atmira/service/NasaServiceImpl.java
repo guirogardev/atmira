8d2594302c51c60a1c72277b95e2306dd305fae0
Guille R. Dev <guirogardev@gmail.com>
2022-02-02 18:42:10 +0100

    [develop] Conexión exitosa con la NASA

--- src/main/java/atmira/service/NasaServiceImpl.java
+++ src/main/java/atmira/service/NasaServiceImpl.java
@@ -3,14 +3,21 @@ package atmira.service;
 import java.time.LocalDate;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NasaServiceImpl implements NasaService {
+	
+	private static final String NASA_API_KEY = "zdUP8ElJv1cehFM0rsZVSQN7uBVxlDnu4diHlLSb";
 
 	@Override
 	public void retrieveAsteroidsInfo(LocalDate start, LocalDate end) {
-		System.out.println("Ejecutando retrieveAsteroidsInfo(LocalDate start, LocalDate end)");
-		System.out.println("Fin de retrieveAsteroidsInfo(LocalDate start, LocalDate end)");
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String uri = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-12-09&end_date=2021-12-12&api_key=" + NASA_API_KEY;
+	    String result = restTemplate.getForObject(uri, String.class);
+	    
+	    System.out.println(result);
 	}
 
 }
